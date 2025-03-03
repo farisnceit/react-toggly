9b8c42c3743a17c07113ec10c5f821afcc1d806f
Mohamed Fariz <faris872010@gmail.com>
2025-03-03 15:15:43 +0530

    [1.2.1] working on demo page

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 import ToggleSwitch from './components/Toggle/Toggle';
+export { default as Demo } from './demo/Demo';
 export default ToggleSwitch;
 export type { ToggleProps } from './components/Toggle/types';
